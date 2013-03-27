a94322530e7e8d2ae8f132620e643f2814f707f1
David Kinzer <dtkinzer@gmail.com>
2013-03-27 12:06:46 -0400

    Add kitchen related aliases.

--- heavens-kitchen.sh
+++ heavens-kitchen.sh
@@ -34,6 +34,19 @@ then
   rvm reinstall all --force 
 fi
 
+# Add some kitchen related aliases.
+curl https://raw.github.com/dkinzer/heavens-kitchen/master/.bash_hk -o ~/.bash_hk
+has_kitchen_aliases=$(grep '.bash_hk' ~/.bashrc | head -n 1)
+if [ -z "$has_kitchen_aliases" ];
+then 
+  echo "
+# Load Heaven's Kitchen Aliases
+if [ -f ~/.bash_hk ];
+ then
+   source ~/.bash_hk
+fi" >> ~/.bashrc 
+fi
+
 # Set vim as our git editor.
 git config --global core.editor "vim"
 
@@ -45,3 +58,4 @@ gconftool-2 --set "/apps/gnome-terminal/profiles/Default/use_theme_colors" --typ
 gconftool-2 --set "/apps/gnome-terminal/profiles/Default/palette" --type string "#070736364242:#D3D301010202:#858599990000:#B5B589890000:#26268B8BD2D2:#D3D336368282:#2A2AA1A19898:#EEEEE8E8D5D5:#00002B2B3636:#CBCB4B4B1616:#58586E6E7575:#65657B7B8383:#838394949696:#6C6C7171C4C4:#9393A1A1A1A1:#FDFDF6F6E3E3"
 gconftool-2 --set "/apps/gnome-terminal/profiles/Default/background_color" --type string "#00002B2B3636"
 gconftool-2 --set "/apps/gnome-terminal/profiles/Default/foreground_color" --type string "#65657B7B8383"
+
