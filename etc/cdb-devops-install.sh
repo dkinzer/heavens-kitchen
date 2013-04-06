c78eca099ed40976bbae12fa329ce9287dff0f5d
David Kinzer <dtkinzer@gmail.com>
2013-04-05 23:06:54 -0400

    Add log to cdb install.

--- etc/cdb-devops-install.sh
+++ etc/cdb-devops-install.sh
@@ -4,18 +4,18 @@
 
 wget -qO - https://raw.github.com/dkinzer/heavens-kitchen/master/heavens-kitchen.sh |  bash
 
-# Create a project direcotory.
+log "Create a project direcotory."
 if [ ! -d ~/projects ]; then
   mkdir ~/projects
 fi
 
 # Clone and start up a kitchen project.
 if [ ! -d ~/projects/cdb-devops ]; then
-  # Clone the cdb-devops project.
+  log "Clone the cdb-devops project."
   cd ~/projects
   git clone git@github.com:jenkinslaw/cdb-devops.git
     
-  # Start up the project.
+  log "Start up the project."
   cd cdb-devops
   if [ ! -f solo.rb ]; then
     knife solo init .
